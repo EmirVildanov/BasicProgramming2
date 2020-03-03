dccbc6c96a63470dd56e1ab09c4abdf132afe60a
EmirVildanov <reddog201030@gmail.com>
2020-03-03 21:31:39 +0300

    Added task1.5

---
+++ hw1/task1.5/src/Main.kt
@@ -0,0 +1,21 @@
+import java.io.File
+
+fun countNonEmptyLines(file: File): Int {
+    var answer = 0
+    for (line in file.readLines()) {
+        if (line.isBlank()) {
+            answer++
+        }
+    }
+    return answer
+}
+
+fun main() {
+    val file =  File("test.txt")
+    if (!file.exists()) {
+        print("\nFile not found\n")
+        return
+    }
+    val numberOfLines = countNonEmptyLines(file)
+    println("The number of non-empty lines in your file is : $numberOfLines")
+}
---
+++ hw1/task1.5/src/MainKtTest.kt
@@ -0,0 +1,15 @@
+import org.junit.Test
+import kotlin.test.assertEquals
+
+internal class MainKtTest {
+
+//    @Test
+//    fun countNonEmptyLines() {
+//        val string = "firstLine/n"
+//    }
+//
+//    @Test
+//    fun shouldReturnZeroOnOnlyEmptyLine() {
+//        assertEquals(0, countNonEmptyLines("\n"))
+//    }
+}
