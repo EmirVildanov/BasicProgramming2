9be658b41f260bb766c3bcf0be89c74b26991a81
EmirVildanov <reddog201030@gmail.com>
2020-03-15 20:46:46 +0300

    Now it's partly working

--- hw3/task3.1/src/AVLTree.kt
+++ hw3/task3.1/src/AVLTree.kt
@@ -1,7 +1,4 @@
-import java.util.Comparator
-
 class AVLTree<K, V>(
-    //val compareFunction: (firstValue: K?, secondValue: K?) . Int,
     private var root: Pair<K, V>? = null,
     override val entries: MutableSet<Map.Entry<K, V>> = mutableSetOf(),
     override val keys: MutableSet<K> = mutableSetOf(),
@@ -10,14 +7,14 @@ class AVLTree<K, V>(
 ) : Map<K, V> where K : Comparable<K> {
 
     class Pair<K, V>(
-        override val key: K,
+        override var key: K,
         override var value: V,
-        var height: Int = 0,
+        var height: Int = 1,
         var leftChild: Pair<K, V>? = null,
         var rightChild: Pair<K, V>? = null,
         var parent: Pair<K, V>? = null
     ) : Map.Entry<K, V> {
-        
+
     }
 
     override fun containsKey(key: K): Boolean {
@@ -33,42 +30,63 @@ class AVLTree<K, V>(
     }
 
     override fun get(key: K): V? {
-        TODO("not implemented")
+        if (root == null) {
+            return null
+        }
+        var currentPair = root;
+        while (true) {
+            if (currentPair == null) {
+                return null
+            }
+            if (currentPair.key == key) {
+                return currentPair.value
+            } else if (key < currentPair.key) {
+                currentPair = currentPair.leftChild;
+            } else {
+                currentPair = currentPair.rightChild;
+            }
+        }
     }
 
     fun put(key: K, value: V): V? {
         val newPair = Pair(key, value)
         if (isEmpty()) {
             root = newPair
+            entries.add(newPair)
             size++
             return null
         }
         var currentPair = root
         while (true) {
-            if (key.compareTo(currentPair?.key) == -1)//<
+            if (currentPair == null) {
+                return null
+            }
+            if (key.compareTo(currentPair.key) == -1)//<
             {
-                if (currentPair?.leftChild == null) {
-                    currentPair?.leftChild = newPair
+                if (currentPair.leftChild == null) {
+                    currentPair.leftChild = newPair
                     newPair.parent = currentPair
                     balanceParents(newPair)
                     ++size
+                    entries.add(newPair)
                     return null
                 }
                 currentPair = currentPair.leftChild
-            } else if (key.compareTo(currentPair?.key) == 1)//>
+            } else if (key.compareTo(currentPair.key) == 1)//>
             {
-                if (currentPair?.rightChild == null) {
-                    currentPair?.rightChild = newPair
+                if (currentPair.rightChild == null) {
+                    currentPair.rightChild = newPair
                     newPair.parent = currentPair
                     balanceParents(newPair)
                     size++
+                    entries.add(newPair)
                     return null
                 }
                 currentPair = currentPair.rightChild
             } else//=
             {
-                val rememberValue = currentPair?.value
-                currentPair?.value = value
+                val rememberValue = currentPair.value
+                currentPair.value = value
                 return rememberValue
             }
         }
@@ -76,25 +94,6 @@ class AVLTree<K, V>(
 
     fun putAll(from: Map<out K, V>) {
         from.forEach { this.put(it.key, it.value) }
-    }
-
-    private fun findElement(key: K): Boolean {
-        if (root == null) {
-            return false;
-        }
-        var currentPair = root;
-        while (true) {
-            if (currentPair == null) {
-                return false;
-            }
-            if (currentPair.key == key) {
-                return true;
-            } else if (key < currentPair.key) {
-                currentPair = currentPair.leftChild;
-            } else {
-                currentPair = currentPair.rightChild;
-            }
-        }
     }
 
     private fun findNewRoot(pair: Pair<K, V>?): Pair<K, V>? {
@@ -212,7 +211,8 @@ class AVLTree<K, V>(
         }
     }
 
-    private fun findElementToDelete(currentPair: Pair<K, V>?, value: K): Pair<K, V>? {
+    private fun findElementToDelete(value: K): Pair<K, V>? {
+        var currentPair = root
         while (true) {
             if (currentPair == null) {
                 return null;
@@ -229,9 +229,11 @@ class AVLTree<K, V>(
     }
 
     private fun deleteRoot(currentPair: Pair<K, V>?) {
-        if (currentPair?.leftChild == null && currentPair?.rightChild == null) {
+        if (currentPair == null) {
+            return
+        }
+        if (currentPair.leftChild == null && currentPair.rightChild == null) {
             root = null;
-
             --size;
         } else if (currentPair.leftChild == null) {
             root = currentPair.rightChild;
@@ -240,14 +242,18 @@ class AVLTree<K, V>(
             root = currentPair.leftChild;
             size
         } else {
-            val newRoot = findNewRoot(currentPair);
-            val rememberValue = newRoot?.value;
-            deleteElement(rememberValue);
-            currentPair.value = rememberValue;
+            val newRoot = findNewRoot(currentPair) ?: return;
+            val rememberKey = newRoot.key;
+            remove(rememberKey);
+            currentPair.key = rememberKey;
+            currentPair.value = newRoot.value
         }
     }
 
     private fun deleteLeftChild(currentPair: Pair<K, V>?) {
+        if (currentPair == null) {
+            return
+        }
         if (currentPair.leftChild == null && currentPair.rightChild == null) {
             currentPair.parent?.leftChild = null;
             --size;
@@ -260,16 +266,19 @@ class AVLTree<K, V>(
             currentPair.leftChild?.parent = currentPair.parent;
             --size;
         } else {
-            val newRoot = findNewRoot(currentPair);
-            val rememberValue = newRoot?.value;
-            deleteElement(rememberValue);
-            currentPair.value = rememberValue;
+            val newRoot = findNewRoot(currentPair) ?: return;
+            val rememberKey = newRoot.key;
+            remove(rememberKey);
+            currentPair.key = rememberKey;
         }
     }
 
     private fun deleteRightChild(currentPair: Pair<K, V>?) {
-        if (currentPair?.leftChild == null && currentPair?.rightChild == null) {
-            currentPair?.parent?.rightChild = null;
+        if (currentPair == null) {
+            return
+        }
+        if (currentPair.leftChild == null && currentPair.rightChild == null) {
+            currentPair.parent?.rightChild = null;
             --size;
         } else if (currentPair.leftChild == null) {
             currentPair.parent?.rightChild = currentPair.rightChild;
@@ -280,15 +289,17 @@ class AVLTree<K, V>(
             currentPair.leftChild?.parent = currentPair.parent;
             --size;
         } else {
-            val newRoot = findNewRoot(currentPair);
-            val rememberValue = newRoot?.value;
-            deleteElement(rememberValue);
-            currentPair.value = rememberValue;
+            val newRoot = findNewRoot(currentPair) ?: return
+            val rememberKey = newRoot.key;
+            remove(rememberKey);
+            currentPair.key = rememberKey;
+            currentPair.value = newRoot.value
         }
     }
 
     fun remove(key: K): K? {
-        val currentPair = findElementToDelete(root, key); //Finding element
+        val currentPair = findElementToDelete(key); //Finding element
+        val currentKey = currentPair?.key
         var rememberCurrentElementParent =
             currentPair?.parent;//we need it to balance parents after deletion current element
         if (currentPair == root)//If the element is a root
@@ -308,9 +319,31 @@ class AVLTree<K, V>(
         while (rememberCurrentElementParent?.parent != null) {
             rememberCurrentElementParent = balance(rememberCurrentElementParent.parent);
         }
+        return currentKey
     }
 
     fun clear() {
         entries.forEach { remove(it.key) }
+    }
+
+    private fun processPrint(pair: Pair<K, V>?) {
+        if (pair == null)
+        {
+            print("null ")
+        }
+        else
+        {
+            print("(")
+            print("${pair.key} ")
+            processPrint(pair.leftChild)
+            processPrint(pair.rightChild)
+            print(") ")
+        }
+    }
+
+    fun print() {
+        print("The hole set : ")
+        processPrint(root)
+        print("\n")
     }
 }
