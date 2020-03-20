b9517d9d41c52f0527bf8931cc3f80557b1013ec
EmirVildanov <reddog201030@gmail.com>
2020-03-20 12:54:48 +0300

    Added new kotlin class

---
+++ src/main/kotlin/hw5/task5.1/Trie.kt
@@ -0,0 +1,33 @@
+import java.io.InputStream
+import java.io.OutputStream
+
+class Trie {
+
+    val size = 0
+        get() = field
+
+    fun add(element: String): Boolean {
+        TODO()
+
+    }
+
+    fun contains(element: String): Boolean {
+        TODO()
+    }
+
+    fun remove(element: String): Boolean {
+        TODO()
+    }
+
+    fun howManyStartWithPrefix(prefix: String): Int {
+        TODO()
+    }
+
+    fun serialize(out: OutputStream) {
+
+    }
+
+    fun deserialize(input: InputStream) {
+
+    }
+}
