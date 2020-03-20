182c36d324392c25ce9ce6d2dca32aa74c1747c5
EmirVildanov <reddog201030@gmail.com>
2020-03-20 13:46:15 +0300

    Reorganized structure and added tests

--- src/main/kotlin/hwFirst/taskFive/src/Main.kt
+++ src/main/kotlin/hwFirst/taskFive/src/Main.kt
@@ -3,7 +3,7 @@ import java.io.File
 fun countNonEmptyLines(file: File): Int {
     var answer = 0
     for (line in file.readLines()) {
-        if (line.isBlank()) {
+        if (!line.isBlank()) {
             answer++
         }
     }
@@ -11,7 +11,7 @@ fun countNonEmptyLines(file: File): Int {
 }
 
 fun main() {
-    val file = File("test.txt")
+    val file = File("src/main/kotlin/hwFirst/taskFive/test.txt")
     if (!file.exists()) {
         print("\nFile not found\n")
         return
---
+++ src/test/kotlin/hwFirst/taskFive/MainKtTest.kt
@@ -0,0 +1,29 @@
+package hwFirst.taskFive
+
+import countNonEmptyLines
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.io.File
+
+internal class MainKtTest {
+
+    @Test
+    fun shouldReturn0OnEmptyFile() {
+        assertEquals(0, countNonEmptyLines(File("src/test/kotlin/hwFirst/taskFive/testEmptyFile")))
+    }
+
+    @Test
+    fun shouldReturn3OnFileWith3NonEmptyLines() {
+        assertEquals(3, countNonEmptyLines(File("src/test/kotlin/hwFirst/taskFive/testFileWith3NonEmptyLines")))
+    }
+
+    @Test
+    fun shouldReturn1OnFileWithOneNonEmptyLineAndNewLineSymbol() {
+        assertEquals(1, countNonEmptyLines(File("src/test/kotlin/hwFirst/taskFive/testFileWithEmptyLastLine")))
+    }
+
+    @Test
+    fun shouldReturn7OnFileWithManyEmptyLinesContainingDifferentEmptySymbols() {
+        assertEquals(7, countNonEmptyLines(File("src/test/kotlin/hwFirst/taskFive/testFileWithManyEmptyLinesContainingDifferentEmptySymbols")))
+    }
+}
