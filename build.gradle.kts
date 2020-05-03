6f8b80a88324e4824e64412a78b2c484fa068338
EmirVildanov <reddog201030@gmail.com>
2020-05-03 14:04:38 +0500

    Deleted useless lines in gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,20 +21,9 @@ dependencies {
     implementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     implementation("org.junit.jupiter:junit-jupiter-engine:5.6.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.5")
-//    implementation("de.jensd:fontawesomefx-weathericons:2.0.10-9.1.2")
     implementation("no.tornado:tornadofx:1.7.20")
-
-//    compile("org.codehaus.griffon:griffon-javafx:14")
-//    compile("no.tornado:tornadofx:1.7.17")
-//    implementation("org.controlsfx:controlsf.x:11.0.1")
-
     detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.6.0")
 }
-
-//javafx {
-//    version = "14"
-//    modules("javafx.controls")
-//}
 
 detekt {
     failFast = true // fail build on any finding
