393fc0d13f62813ee00778b267bd6f0a304f4a00
EmirVildanov <reddog201030@gmail.com>
2020-05-18 20:09:20 +0500

    Added null checking

--- src/main/kotlin/homework/hw1/task1/Main.kt
+++ src/main/kotlin/homework/hw1/task1/Main.kt
@@ -1,11 +1,11 @@
 package homework.hw1.task1
 
-fun rearrangeArrayParts(array: IntArray, firstBoard: Int): IntArray {
-    val listArray = array.toMutableList()
-    listArray.subList(0, firstBoard).reverse()
-    listArray.subList(firstBoard, array.size).reverse()
-    listArray.subList(0, array.size).reverse()
-    return listArray.toIntArray()
+import kotlin.collections.toMutableList
+
+fun rearrangeArrayParts(array: MutableList<Int>, firstBoard: Int) {
+    array.subList(0, firstBoard).reverse()
+    array.subList(firstBoard, array.size).reverse()
+    array.subList(0, array.size).reverse()
 }
 
 fun main() {
@@ -13,12 +13,13 @@ fun main() {
     val firstBoard = readLine()?.toInt()
     print("Enter the second board : ")
     val secondBoard = readLine()?.toInt()
-    val length = firstBoard!! + secondBoard!!
-    val array = IntArray(length) { 0 }
-    print("Enter $length elements of the list(every from the new line) : ")
-    for (i in 0 until length) {
-        array[i] = readLine()?.toInt()!!
+    if (firstBoard != null && secondBoard != null) {
+        val length = firstBoard + secondBoard
+        print("Enter $length elements of the list : ")
+        val array: MutableList<Int>? = readLine()?.split(' ')?.map { it.toInt() }?.toMutableList()
+        if (array != null) {
+            rearrangeArrayParts(array, firstBoard)
+            print("The answer is : ${array.joinToString(" ")}")
+        }
     }
-    rearrangeArrayParts(array, firstBoard)
-    print("The answer is : ${array.joinToString(" ")}")
 }
--- src/test/kotlin/homework/hw1/task1/MainKtTest.kt
+++ src/test/kotlin/homework/hw1/task1/MainKtTest.kt
@@ -2,13 +2,59 @@ package homework.hw1.task1
 
 import org.junit.jupiter.api.Test
 
-import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Assertions.assertEquals
 
 internal class MainKtTest {
 
     @Test
+    fun shouldDoNothingWithIntArray() {
+        val array = mutableListOf(1, 2, 3, 4, 5)
+        rearrangeArrayParts(array, array.size)
+        println(array)
+        for (i in array.indices) {
+            assertEquals(intArrayOf(1, 2, 3, 4, 5)[i], array[i])
+        }
+    }
+
+    @Test
+    fun shouldLeaveIntArrayInHisFirstView() {
+        val array = mutableListOf(1, 2, 3, 4, 5)
+        rearrangeArrayParts(array, 0)
+        for (i in array.indices) {
+            assertEquals(intArrayOf(1, 2, 3, 4, 5)[i], array[i])
+        }
+    }
+
+    @Test
+    fun shouldAlsoLeaveIntArrayInHisFirstView() {
+        val array = mutableListOf(1, 2, 3, 4, 5)
+        rearrangeArrayParts(array, 0)
+        for (i in array.indices) {
+            assertEquals(intArrayOf(1, 2, 3, 4, 5)[i], array[i])
+        }
+    }
+
+    @Test
+    fun shouldRearrangeArrayParts() {
+        val array = mutableListOf(1, 2, 3, 4, 5)
+        rearrangeArrayParts(array, 3)
+        for (i in array.indices) {
+            assertEquals(intArrayOf(4, 5, 1, 2, 3)[i], array[i])
+        }
+    }
+
+    @Test
+    fun shouldRearrangePartsOfNegativeArray() {
+        val array = mutableListOf(-1, -2, -3, -4, -5)
+        rearrangeArrayParts(array, 2)
+        for (i in array.indices) {
+            assertEquals(intArrayOf(-3, -4, -5, -1, -2)[i], array[i])
+        }
+    }
+
+    @Test
     fun shouldRearrangeEasyArray() {
-        val testArray = IntArray(6) { it + 1 }
+        val testArray = IntArray(6) { it + 1 }.toMutableList()
         val assertingArray = IntArray(6)
         assertingArray[0] = 4
         assertingArray[1] = 5
@@ -16,12 +62,15 @@ internal class MainKtTest {
         assertingArray[3] = 1
         assertingArray[4] = 2
         assertingArray[5] = 3
-        assertArrayEquals(assertingArray, rearrangeArrayParts(testArray, 3))
+        rearrangeArrayParts(testArray, 3)
+        for (i in testArray.indices) {
+            assertEquals(assertingArray[i], testArray[i])
+        }
     }
 
     @Test
     fun shouldRearrangeMediumSizeArray() {
-        val testArray = IntArray(10) { it + 1 }
+        val testArray = IntArray(10) { it + 1 }.toMutableList()
         val assertingArray = IntArray(10)
         assertingArray[0] = 8
         assertingArray[1] = 9
@@ -33,6 +82,9 @@ internal class MainKtTest {
         assertingArray[7] = 5
         assertingArray[8] = 6
         assertingArray[9] = 7
-        assertArrayEquals(assertingArray, rearrangeArrayParts(testArray, 7))
+        rearrangeArrayParts(testArray, 7)
+        for (i in testArray.indices) {
+            assertEquals(assertingArray[i], testArray[i])
+        }
     }
 }
