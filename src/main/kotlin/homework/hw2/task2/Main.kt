1b936a0dd3fbad07c8276786c871f428de49d7d7
EmirVildanov <reddog201030@gmail.com>
2020-05-31 21:50:18 +0500

    Trying to use features to solve task

--- src/main/kotlin/homework/hw2/task2/Main.kt
+++ src/main/kotlin/homework/hw2/task2/Main.kt
@@ -1,40 +1,15 @@
-import java.util.Scanner
 
-fun deleteRepeatingElements(array: MutableList<Int>): Array<Int> {
-    array.reverse()
-    val set = array.toSet()
-    set.reversed().forEach {
-        print("$it ")
-    }
-//    var index: Int
-//    for (i in 0 until array.size) {
-//        if (i == array.size) {
-//            break
-//        }
-//        index = i + 1
-//        while (index < array.size) {
-//            if (array[i] == array[index]) {
-//                array.removeAt(index)
-//                index--
-//            }
-//            index++
-//        }
-//    }
-//    array.reverse()
-    return set.reversed().toTypedArray()
+fun deleteRepeatingElements(array: MutableList<Int>): MutableList<Int> {
+    return array.reversed().toSet().reversed().toMutableList()
 }
 
 fun main() {
-    val scan = Scanner(System.`in`)
-    print("Enter the length of your array : ")
-    val length: Int = scan.nextInt()
-    print("Enter $length elements of the array : ")
+    print("Enter elements of the array : ")
     val array = readLine()?.split(' ')?.map { it.toInt() }?.toMutableList()
     if (array != null) {
-        val array = deleteRepeatingElements(array)
         print("Your array after transformation is : ")
-        array.forEach { print("$it ") }
+        deleteRepeatingElements(array).forEach { print("$it ") }
     } else {
-        println("Input array is empty")
+        println("Input array is null")
     }
 }
--- src/test/kotlin/homework/hw2/task2/MainKtTest.kt
+++ src/test/kotlin/homework/hw2/task2/MainKtTest.kt
@@ -13,35 +13,30 @@ internal class MainKtTest {
     @Test
     fun shouldLeaveOnlyOneUnit() {
         val array = arrayListOf(1, 1, 1, 1)
-        deleteRepeatingElements(array)
-        assertEquals(array, arrayListOf(1))
+        assertEquals(deleteRepeatingElements(array), arrayListOf(1))
     }
 
     @Test
     fun shouldLeaveFirstThreeNumbers() {
         val array = arrayListOf(1, 11, 23, 23, 23, 23, 23)
-        deleteRepeatingElements(array)
-        assertEquals(array, arrayListOf(1, 11, 23))
+        assertEquals(deleteRepeatingElements(array), arrayListOf(1, 11, 23))
     }
 
     @Test
     fun shouldLeaveOnlyOneUnitInTheCenter() {
         val array = arrayListOf(2, 1, 1, 1, 1, 1, 3)
-        deleteRepeatingElements(array)
-        assertEquals(array, arrayListOf(2, 1, 3))
+        assertEquals(deleteRepeatingElements(array), arrayListOf(2, 1, 3))
     }
 
     @Test
     fun shouldLeaveAllNumbersOnes() {
         val array = arrayListOf(1, 1, 2, 2, 3, 3, 4, 4)
-        deleteRepeatingElements(array)
-        assertEquals(array, arrayListOf(1, 2, 3, 4))
+        assertEquals(deleteRepeatingElements(array), arrayListOf(1, 2, 3, 4))
     }
 
     @Test
     fun shouldLeaveOnlyTheRightPartOfTheMirroring() {
         val array = arrayListOf(4, 3, 2, 1, 1, 2, 3, 4)
-        deleteRepeatingElements(array)
-        assertEquals(array, arrayListOf(1, 2, 3, 4))
+        assertEquals(deleteRepeatingElements(array), arrayListOf(1, 2, 3, 4))
     }
 }
