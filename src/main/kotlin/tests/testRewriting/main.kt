70f80494f504e3c92c261f9e9969eaffe70c5d88
EmirVildanov <reddog201030@gmail.com>
2020-11-29 19:27:50 +0300

    Created test rewriting branch

---
+++ src/main/kotlin/tests/testRewriting/main.kt
@@ -0,0 +1,5 @@
+package tests.testRewriting
+
+fun main() {
+    println("Test")
+}
---
+++ src/test/kotlin/tests/testRewriting/RewritingTest.kt
@@ -0,0 +1,12 @@
+package tests.testRewriting
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+internal class RewritingTest {
+    @Test
+    fun shouldAssertTwoValues() {
+        assertEquals(5, 5)
+    }
+}
